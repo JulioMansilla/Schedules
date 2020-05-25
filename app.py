fa6813a0630443d8c64653bde195577a36aa33ac
JulioMansilla <mansilla.jb@gmail.com>
2020-05-25 16:50:52 -0400

    Add routes and render_template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #special python variable with the name of the module
 app=Flask(__name__)
@@ -6,5 +6,14 @@ app=Flask(__name__)
 
 #define routes
 @app.route("/")
-def hello():
-    return "<h1>??????</h1>"
+@app.route("/landing")
+def landing():
+    return render_template('landing.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
